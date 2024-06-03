ddce043a296f0e3477b5308cbf7bcc93e1cba35a
Ngo Nam <namngo102003@gmail.com>
2024-06-03 23:24:42 +0700

    refactor code

--- screens/ForgotScreen.js
+++ screens/ForgotScreen.js
@@ -6,9 +6,10 @@ import Button from "../components/button";
 import { colors } from "../theme";
 import { useNavigation } from "@react-navigation/native";
 import { ForgotPassword } from "../controller/ForgotController";
-import Toast from "react-native-toast-message";
 import { useDispatch } from "react-redux";
 import { setOTP } from "../redux/slices/otpSlice";
+import ShowToast from "../components/toast";
+import * as Icons from "react-native-heroicons/outline";
 
 const { width, height } = Dimensions.get("window");
 
@@ -18,12 +19,7 @@ const ForgotScreen = () => {
     const [email, setEmail] = useState("");
     const handleVerifyEmail = async () => {
         const result = await ForgotPassword(email);
-        Toast.show({
-            type: "success",
-            text1: result[1],
-            topOffset: 70,
-            text1Style: { fontSize: 18 },
-        });
+        ShowToast("success", "Thông báo", result[1])
 
         if (result[0]) {
             dispatch(setOTP(result[2]))
@@ -36,7 +32,11 @@ const ForgotScreen = () => {
         <View className="flex-1 justify-center items-center">
             <Image source={require("../assets/images/bgOther.png")} style={{ width: width, height: height }} />
 
-            <View className="absolute top-52">
+            <TouchableOpacity onPress={() => navigation.goBack()} className='absolute top-12 left-5 p-3 rounded-full' style={{backgroundColor: colors.primary}}>
+                <Icons.ChevronLeftIcon size={24} color={'white'} />
+            </TouchableOpacity>
+
+            <View className="absolute" style={{top: hp(25)}}>
                 <View className="flex justify-center items-center">
                     <Text className="text-3xl font-bold">Bạn quên mật khẩu ư?</Text>
                     <Text>Đừng lo, chúng tôi sẽ hỗ trợ bạn</Text>
